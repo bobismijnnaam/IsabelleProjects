238bfb155032444d0fee5e4206fbd423c6016281
bobismijnnaam <bobrubbens@gmail.com>
2020-03-16 17:48:50 +0100

    Skipped proving the existential, and moved on to proving the (easier) later stuff

--- Quantifying_Lists.thy
+++ Quantifying_Lists.thy
@@ -42,13 +42,45 @@ qed
 lemma nex_all: "\<not>(\<exists>x. P x) \<equiv> \<forall> x. \<not>P x"
   by simp
 
+(* Disproven by counterexample
 lemma "\<exists>P Q xs. exs (\<lambda>x. P x \<and> Q x) xs \<noteq> (exs P xs \<and> exs Q xs)"
 proof(rule ccontr)
   assume "\<not>?thesis"
   then have "\<forall>P Q xs. \<not>(exs (\<lambda>x. P x \<and> Q x) xs \<noteq> (exs P xs \<and> exs Q xs))"
-    apply(simp only: nex_all)
-    
+    apply(simp only: nex_all) *)
 
+lemma "exs P (map f xs) = exs (P o f) xs"
+  by(induct xs, simp+)
 
+lemma [simp]: "exs P (xs @ ys) = (exs P xs \<or> exs P ys)"
+  by(induct xs, auto)
+
+lemma "exs P (rev xs) = exs P xs"
+proof(induct xs)
+case Nil
+  then show ?case by simp
+next
+case (Cons a xs)
+  then show ?case by(auto)
+qed
+
+lemma "exs (\<lambda>x. P x \<or> Q x) xs = (exs P xs \<or> exs Q xs)"
+proof(induct xs)
+case Nil
+  then show ?case by simp
+next
+  case (Cons a xs)
+  then show ?case by auto
+qed
+
+lemma "exs P xs = (\<not>(\<forall>x\<in>set xs.\<not>(P x)))"
+  by(induct xs, auto)
+
+primrec is_in :: "'a \<Rightarrow> 'a list \<Rightarrow> bool" where
+"is_in _ Nil = False" | 
+"is_in v (Cons x xs) = (if v = x then True else is_in v xs)"
+
+lemma "is_in a xs = exs (\<lambda>x. x = a) xs"
+  by(induct xs, auto)
 
 end
