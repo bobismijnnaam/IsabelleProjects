ae777c98339ef2f47beeded297af16ee41ecb52d
bobismijnnaam <bobrubbens@gmail.com>
2020-03-18 09:59:51 +0100

    Finished Searching in Lists!

--- Searching_In_Lists.thy
+++ Searching_In_Lists.thy
@@ -17,4 +17,30 @@ lemma "(first_pos P xs = size xs) = (\<forall> x \<in> set xs. \<not>(P x))"
 lemma "list_all (\<lambda>x. \<not>P x) (take (first_pos P xs) xs)"
   by(induct xs, auto)
 
+lemma "first_pos (\<lambda>x. P x \<or> Q x) xs = min (first_pos P xs) (first_pos Q xs)"
+  by(induction xs, auto)
+
+lemma "first_pos (\<lambda>x. P x \<and> Q x) xs \<ge> max (first_pos P xs) (first_pos Q xs)"
+  by(induction xs, auto)
+
+lemma
+  assumes "\<forall>x. (P x \<longrightarrow> Q x)" 
+  shows "first_pos P xs < length xs \<longrightarrow> first_pos Q xs < length xs"
+  using assms by (induction xs, auto)
+
+fun count :: "('a \<Rightarrow> bool) \<Rightarrow> 'a list \<Rightarrow> nat" where
+"count _ Nil = 0" |
+"count f (Cons x xs) = (if f x then 1 else 0) + count f xs"
+
+print_theorems  
+
+lemma count_concat_split: "count f (xs @ ys) = count f xs + count f ys"
+  by(induct xs, auto)
+
+lemma "count f xs = count f (rev xs)"
+  by(induct xs, auto simp add: count_concat_split)
+
+lemma "length (filter f xs) = count f xs"
+  by(induct xs, auto)
+
 end
